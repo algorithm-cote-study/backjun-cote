a64b4eb93c47a3db9ef59d0231f3f2fbf287fae5
cyeji <98408267+cyeji@users.noreply.github.com>
2025-10-10 08:45:18 +0900

    [Bronze V] Title: 나머지, Time: 108 ms, Memory: 14364 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] array = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int a = array[0];
+        int b = array[1];
+        int c = array[2];
+
+        System.out.println((a+b)%c);
+        System.out.println(((a%c)+(b%c))%c);
+        System.out.println((a*b)%c);
+        System.out.println(((a%c)*(b%c))%c);
+    }
+
+}
